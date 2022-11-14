344b52e2551a59adab23b08670bd9b0be8941cbb
Stephen Domican <57364979+SDomican@users.noreply.github.com>
2022-11-14 16:21:51 +0100

    Update

--- myJavascript.js
+++ myJavascript.js
@@ -85,16 +85,16 @@ function loadVideo(){
     // console.log("merge: " + test);
 }
 
-async function mergeVideo(video, audio) {
-    let { createFFmpeg, fetchFile } = FFmpeg;
-    let ffmpeg = createFFmpeg();
-    await ffmpeg.load();
-    ffmpeg.FS('writeFile', 'video.mp4', await fetchFile(video));
-    ffmpeg.FS('writeFile', 'audio.mp4', await fetchFile(audio));
-    await ffmpeg.run('-i', 'video.mp4', '-i', 'audio.mp4', '-c', 'copy', 'output.mp4');
-    let data = await ffmpeg.FS('readFile', 'output.mp4');
-    return new Uint8Array(data.buffer);
-};
+// async function mergeVideo(video, audio) {
+//     let { createFFmpeg, fetchFile } = FFmpeg;
+//     let ffmpeg = createFFmpeg();
+//     await ffmpeg.load();
+//     ffmpeg.FS('writeFile', 'video.mp4', await fetchFile(video));
+//     ffmpeg.FS('writeFile', 'audio.mp4', await fetchFile(audio));
+//     await ffmpeg.run('-i', 'video.mp4', '-i', 'audio.mp4', '-c', 'copy', 'output.mp4');
+//     let data = await ffmpeg.FS('readFile', 'output.mp4');
+//     return new Uint8Array(data.buffer);
+// };
 
 function playBoth(url, filename) {
     alert("Play Both function activated: " + vid1);
